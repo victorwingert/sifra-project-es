cc5082cf3ee63ef2da1a600718102330ea735d7a
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2026-01-19 00:07:43 -0300

    Integra frontend com nova api

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+const api = axios.create({
+  baseURL: "http://5.78.68.22:5070/api/v1",
 });
 
-export default instance;
+api.interceptors.request.use((config) => {
+  if (!config.url.includes("/auth/token")) {
+    const usuario = localStorage.getItem("usuarioLogado");
+
+    if (usuario) {
+      const { access_token } = JSON.parse(usuario);
+      if (access_token) {
+        config.headers.Authorization = `Bearer ${access_token}`;
+      }
+    }
+  }
+
+  return config;
+});
+
+
+export default api;
